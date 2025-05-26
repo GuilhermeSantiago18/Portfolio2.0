cd7aa00586c55dc6df74aef36ad488479fd833a6
Guilherme <guilherme.limasantiago@gmail.com>
2025-05-26 05:54:20 -0300

    initial setup tests for frontend

---
+++ frontend/src/components/Header/Header.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import Header from './Header'
+
+describe('Header', () => {
+  it('Should render portfolio title"', () => {
+    render(<Header />)
+    expect(screen.getByText('My portfolio')).toBeDefined()
+  })
+})
