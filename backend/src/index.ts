a036bdc8866733df3245b6979ae5eaf1ef14a26d
Guilherme <guilherme.limasantiago@gmail.com>
2025-05-27 15:41:12 -0300

    update app listen

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,6 +23,6 @@ app.use('/api', chatRoutes);
 app.use(errorHandler)
 
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(3000, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
