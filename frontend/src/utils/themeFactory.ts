6874be5b3c181861ad12f6651d3a4d2116616a25
Guilherme <guilherme.limasantiago@gmail.com>
2025-05-27 15:52:52 -0300

    fix: optional param theme

--- frontend/src/utils/themeFactory.ts
+++ frontend/src/utils/themeFactory.ts
@@ -2,7 +2,7 @@ import { THEME_LIGHT } from '../constants/theme';
 import { useThemeStore } from '../stores/useThemeStore';
 import { getThemeFromColor } from './themeGenerator';
 
-export const themeFactory = () => {
+export const themeFactory = (_themeType?: string) => {
   const { theme, primaryColor } = useThemeStore.getState();
   const themes = getThemeFromColor(primaryColor);
   return theme === THEME_LIGHT ? themes.light : themes.dark;
