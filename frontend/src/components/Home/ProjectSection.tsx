32884c55d85d50365e0476e37c311b91c4950aa4
Guilherme <guilherme.limasantiago@gmail.com>
2025-05-28 14:15:15 -0300

    feat: update real projects with a new product

--- frontend/src/components/Home/ProjectSection.tsx
+++ frontend/src/components/Home/ProjectSection.tsx
@@ -22,6 +22,12 @@ const projects = [
     playstore: 'https://play.google.com/store/apps/details?id=com.masbienv2.app',
     appstore: 'https://play.google.com/store/apps/details?id=com.masbienv2.app',
   },
+   {
+    name: 'Urupago',
+    image: '/assets/Urupago.png',
+    playstore: 'https://play.google.com/store/apps/details?id=com.masbienv2.app',
+    appstore: 'https://play.google.com/store/apps/details?id=com.masbienv2.app',
+  },
 ];
 
 const ProjectsSection: React.FC = () => {
@@ -29,7 +35,7 @@ const ProjectsSection: React.FC = () => {
 
   return (
     <section id="real-projects" className="mt-20">
-      <div className="w-full max-w-4xl grid gap-6 grid mt-6 md:grid-cols-3 gap-4">
+      <div className="w-full max-w-4xl grid gap-6 grid mt-6 md:grid-cols-4 gap-4">
         {projects.map((project) => (
           <ProjectCard key={project.name} {...project} />
         ))}
