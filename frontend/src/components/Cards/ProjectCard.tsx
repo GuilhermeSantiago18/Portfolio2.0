ecd429bd89c51459f91023f0c99d40e55fa610df
Guilherme <guilherme.limasantiago@gmail.com>
2025-05-28 14:14:49 -0300

    refactor project card to receive web sites

--- frontend/src/components/Cards/ProjectCard.tsx
+++ frontend/src/components/Cards/ProjectCard.tsx
@@ -4,11 +4,12 @@ import { APP_STORE_BADGE, GOOGLE_PLAY_BADGE } from '../../constants/constants';
 interface ProjectCardProps {
   name: string;
   image: string;
-  playstore: string;
-  appstore: string;
+  playstore?: string;
+  appstore?: string;
+  website?: string;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ name, image, playstore, appstore }) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({ name, image, playstore, appstore, website }) => {
   return (
     <div className="bg-white dark:bg-gray-800 rounded-2xl shadow-lg p-4 flex flex-col items-center border border-gray-200 dark:border-gray-700 transition hover:shadow-xl">
       <h4 className="font-semibold text-xl text-center mb-4 text-black">{name}</h4>
@@ -20,20 +21,34 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ name, image, playstore, appst
       />
 
       <div className="flex gap-4 justify-center items-center">
-        <a href={playstore} target="_blank" rel="noopener noreferrer">
-          <img
-            src={GOOGLE_PLAY_BADGE}
-            alt="Google Play"
-            className="w-32 hover:scale-105 transition"
-          />
-        </a>
-        <a href={appstore} target="_blank" rel="noopener noreferrer">
-          <img
-            src={APP_STORE_BADGE}
-            alt="App Store"
-            className="w-26 hover:scale-105 transition mr-2"
-          />
-        </a>
+        {website ? (
+          <a href={website} target="_blank" rel="noopener noreferrer">
+            <button className="px-4 py-2 bg-blue-600 text-white rounded-xl hover:bg-blue-700 transition">
+              Acessar Site
+            </button>
+          </a>
+        ) : (
+          <>
+            {playstore && (
+              <a href={playstore} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={GOOGLE_PLAY_BADGE}
+                  alt="Google Play"
+                  className="w-32 hover:scale-105 transition"
+                />
+              </a>
+            )}
+            {appstore && (
+              <a href={appstore} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={APP_STORE_BADGE}
+                  alt="App Store"
+                  className="w-26 hover:scale-105 transition mr-2"
+                />
+              </a>
+            )}
+          </>
+        )}
       </div>
     </div>
   );
